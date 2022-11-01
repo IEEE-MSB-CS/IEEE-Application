30d99f2a88478ee9a1662c8dd9f4370189b0cc3d
Hamza Gamal <54688005+hamza94max@users.noreply.github.com>
2022-11-01 16:44:23 +0200

    First commit : setUp the project

---
+++ app/src/main/java/com/ieee/events/ui/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.ieee.events.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ieee.events.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
