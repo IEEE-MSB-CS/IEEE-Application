de6c675de51c65d5cf63d04ad641091a1bf4b16f
Hamza Gamal <54688005+hamza94max@users.noreply.github.com>
2022-11-20 18:54:02 +0200

    HomeFragment ui

--- app/src/main/java/com/ieee/events/ui/MainActivity.kt
+++ app/src/main/java/com/ieee/events/ui/MainActivity.kt
@@ -2,13 +2,16 @@ package com.ieee.events.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.ieee.events.R
+import com.ieee.events.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
 
