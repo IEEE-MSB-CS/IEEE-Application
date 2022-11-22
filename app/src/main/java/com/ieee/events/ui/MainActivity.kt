04c7196fae499b3b94e01a245fb32c49cd9aa762
Ahmed Maher <ahmaher04@gmail.com>
2022-11-22 16:40:26 +0200

    Event fragment ui

--- app/src/main/java/com/ieee/events/ui/MainActivity.kt
+++ app/src/main/java/com/ieee/events/ui/MainActivity.kt
@@ -2,17 +2,13 @@ package com.ieee.events.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.add
-import androidx.fragment.app.commit
 import com.ieee.events.R
-import com.ieee.events.ui.EventFragment.EventFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
 
 
