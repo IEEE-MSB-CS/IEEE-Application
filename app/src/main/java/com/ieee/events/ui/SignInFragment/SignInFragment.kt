898437ef22ae8962898388c7dff6f97e78214e95
noor serry <noor.serry.94@gmail.com>
2022-11-24 14:00:39 +0200

    ""

---
+++ app/src/main/java/com/ieee/events/ui/SignInFragment/SignInFragment.kt
@@ -0,0 +1,22 @@
+package com.ieee.events.ui.SignInFragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ieee.events.R
+import com.ieee.events.databinding.FragmentSignInBinding
+
+
+class SignInFragment : Fragment() {
+    lateinit var binding: FragmentSignInBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSignInBinding.inflate(inflater)
+        return binding.root
+    }
+
+}
